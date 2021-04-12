dd36a76642a41a7dcc909e157b53b00edf63d84c
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-12 15:47:23 -0300

    DS 0016 - Agregando en package.json una dependencia de react hook form llamado yupresolver que es para validar campos de formularios

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,15 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { Form, Alert, Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import { Container, Row, Col} from 'react-bootstrap';
 import { startLoginUser } from './../actions/authActions';
+import { SignInForm } from '../components/SignInForm';
 
-
-const initialState = {
-    email: '',
-    password: ''
-};
 
 
 export const SignIn = () => {
@@ -29,14 +24,10 @@ export const SignIn = () => {
         }
     }, [loggedIn, history]);
 
-    const { 
-        register, 
-        handleSubmit, 
-        formState: { errors } 
-    } = useForm(initialState);
+    
 
 
-    const onSubmit =  ({email, password}) => {
+    const login =  ({email, password}) => {
         const errores = {};
         //Cada vez que se lanze el formulario va a vaciar los errores para volver a recalcular a ver si tiene
         setErrores(errores);
@@ -58,44 +49,7 @@ export const SignIn = () => {
         <Container className="mt-5">
             <Row>
                 <Col sm="12" md={{span: 8, offset: 2}} lg={{span: 6, offset: 3}}>
-                    <Card body bg="dark" text="light">
-                        {
-                            errores.auth && (<Alert variant="danger">{errores.auth}</Alert>)
-                        }
-                        <Card.Title><h2 className="text-align-center">Iniciar sesión</h2></Card.Title>
-                        <hr/>
-                        <Form onSubmit={handleSubmit(onSubmit)}>
-                            <Form.Group control="email">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control
-                                    type="email" 
-                                    placeholder="Correo electronico" 
-                                    className={`form-control bg-white border-left-1 border-md ${errors?.email?.type === "required" && "is-invalid"}`}
-                                    {...register("email", { required: true})}
-                                />
-                            </Form.Group>
-
-                            <Form.Group control="password">
-                                <Form.Label>Contraseña</Form.Label>
-                                <Form.Control
-                                    type="password" 
-                                    placeholder="Contraseña" 
-                                    className={`form-control bg-white border-left-1 border-md ${errors?.password?.type === "required" && "is-invalid"}`}
-                                    {...register("password", { required: true})}
-                                />
-                            </Form.Group>
-                            <Button 
-                                variant="secondary" 
-                                size="lg" block
-                                type="submit"
-                            >
-                                Iniciar sesión
-                            </Button>
-                        </Form>
-                        <div className="mt-4">
-                            <Link to={"/signup"}>Registrarse aqui</Link>
-                        </div>
-                    </Card>
+                    <SignInForm onSubmitCallback={login} errores={errores}/>
                 </Col>
             </Row>
             
