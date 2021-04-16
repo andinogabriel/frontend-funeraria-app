b7135d0dc4107c769c501d256dd7153588988b65
Gabriel Andino <gabriel_andino@outlook.com>
2021-04-16 11:31:30 -0300

    DS 0019 - Agrego la libreria de fontawesome para utilizarla globalmente en la app

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -2,4 +2,11 @@ const API_URL = 'http://localhost:8081/api/v1';
 
 export const USERS_ENDPOINT = API_URL + "/users";
 export const LOGIN_ENDPOINT = API_URL + "/users/login";
+export const USERS_AFFILIATES_ENDPOINT = API_URL + "/users/affiliates";
+export const GENDERS_ENDPOINT = API_URL + "/genders";
+export const RELATIONSHIPS_ENDPOINT = API_URL + "/relationships";
+export const AFFILIATES_ENDPOINT = API_URL + "/affiliates";
+export const SUPPLIERS_ENDPOINT = API_URL + "/suppliers";
+export const MOBILE_NUMBERS_ENDPOINT = API_URL + "/mobileNumbers";
+
 
---
+++ src/helpers/fontawesome.js
@@ -0,0 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit, faTrash, faList, faPlusCircle, faPlusSquare, faMinusCircle, faMinusSquare, faInfoCircle, faPhoneSquareAlt, faPhoneAlt, faEnvelope, faUserTie, faIdCard, faFile } from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+    faEdit,
+    faTrash,
+    faList,
+    faPlusCircle,
+    faPlusSquare,
+    faMinusSquare,
+    faMinusCircle,
+    faInfoCircle,
+    faPhoneSquareAlt,
+    faPhoneAlt,
+    faEnvelope,
+    faUserTie,
+    faIdCard,
+    faFile
+);
